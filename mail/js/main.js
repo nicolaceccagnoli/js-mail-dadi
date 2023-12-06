f9dfae62fe23480a86b8587864dfde0698b1a26e
nicolaceccagnoli <nicola.ceccagnoli.94@gmail.com>
2023-12-06 17:34:58 +0100

    Edit Program (to Fix)

--- mail/js/main.js
+++ mail/js/main.js
@@ -26,19 +26,71 @@
 
     myButton.addEventListener('click', function(){
 
-        // Dichiaro una Variabile in cui inserisco il Valore dell'input dell'utente
+        // Dichiaro una Variabile in cui inserisco il Valore della mail inserita dall'utente
 
         let userMail = (emailInput.value);
 
-        if ( userMail == emailValid[0] || [1] || [2] ) {
+        // 2) Controlla che sia nella lista di chi può accedere;
+
+         if ( userMail == emailValid[0] || [1] || [2] ) {
 
             // Prendo un elemento dal DOM e lo inserisco in una variabile
             const accessApproved = document.getElementById('access-approved');
 
+            accessApproved.innerHTML = `
+
+            <div class="row justify-content-center">
+
+                <div class="col-auto">
+
+                    <div class="card text-bg-light mb-3 text-center">
+                        <div class="card-header">
+                            Permesso di Entrare: 
+                        </div>
+                        <div class="card-body">
+                        <h5 class="card-title text-success">
+                            Concesso
+                        </h5>
+                        </div>
+                    </div>
+
+                </div>
+
+            </div> 
+
+            `;
 
             console.log('agrande');
 
-        } else {
+        } else if ( userMail !== emailValid[0] || [1] || [2] )   {
+
+            // Prendo un elemento dal DOM e lo inserisco in una variabile
+            const accessDenied = document.getElementById('access-denied');
+
+            accessDenied.innerHTML = `
+            
+            <div class="row justify-content-center">
+
+                <div class="col-auto">
+
+                    <div class="card text-bg-light mb-3 text-center">
+                        <div class="card-header">
+                            Permesso di Entrare: 
+                        </div>
+                        <div class="card-body">
+                        <h5 class="card-title text-danger">
+                            Non Concesso
+                        </h5>
+                        <p class='text-warning'> Non puoi entrare! </p>
+                        </div>
+                    </div>
+
+                </div>
+
+            </div>
+
+            `;
+            
 
             console.log('non entra');
         }
