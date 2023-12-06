ba9db65ee63df546e5edd69a7e0b3c614efcd73d
nicolaceccagnoli <nicola.ceccagnoli.94@gmail.com>
2023-12-06 18:17:14 +0100

    Program Fixed

--- mail/js/main.js
+++ mail/js/main.js
@@ -27,73 +27,74 @@
     myButton.addEventListener('click', function(){
 
         // Dichiaro una Variabile in cui inserisco il Valore della mail inserita dall'utente
+    
 
         let userMail = (emailInput.value);
 
-        // 2) Controlla che sia nella lista di chi può accedere;
+        let message = `
+                
+        <div class="row justify-content-center">
 
-         if ( userMail == emailValid[0] || [1] || [2] ) {
+            <div class="col-auto">
 
-            // Prendo un elemento dal DOM e lo inserisco in una variabile
-            const accessApproved = document.getElementById('access-approved');
-
-            accessApproved.innerHTML = `
-
-            <div class="row justify-content-center">
-
-                <div class="col-auto">
-
-                    <div class="card text-bg-light mb-3 text-center">
-                        <div class="card-header">
-                            Permesso di Entrare: 
-                        </div>
-                        <div class="card-body">
-                        <h5 class="card-title text-success">
-                            Concesso
-                        </h5>
-                        </div>
+                <div class="card text-bg-light mb-3 text-center">
+                    <div class="card-header">
+                        Permesso di Entrare: 
                     </div>
-
-                </div>
-
-            </div> 
-
-            `;
-
-            console.log('agrande');
-
-        } else if ( userMail !== emailValid[0] || [1] || [2] )   {
-
-            // Prendo un elemento dal DOM e lo inserisco in una variabile
-            const accessDenied = document.getElementById('access-denied');
-
-            accessDenied.innerHTML = `
-            
-            <div class="row justify-content-center">
-
-                <div class="col-auto">
-
-                    <div class="card text-bg-light mb-3 text-center">
-                        <div class="card-header">
-                            Permesso di Entrare: 
-                        </div>
-                        <div class="card-body">
-                        <h5 class="card-title text-danger">
-                            Non Concesso
-                        </h5>
-                        <p class='text-warning'> Non puoi entrare! </p>
-                        </div>
+                    <div class="card-body">
+                    <h5 class="card-title text-danger">
+                        Non Concesso
+                    </h5>
+                    <p class='text-warning'> Non puoi entrare! </p>
                     </div>
-
                 </div>
 
             </div>
 
-            `;
-            
+        </div>
 
-            console.log('non entra');
+        `;
+
+        // 2) Controlla che sia nella lista di chi può accedere;
+
+        for (let i = 0; i < emailValid.length; i++) {
+
+            if ( userMail == emailValid [i]) {
+    
+                message = `
+    
+                <div class="row justify-content-center">
+    
+                    <div class="col-auto">
+    
+                        <div class="card text-bg-light mb-3 text-center">
+                            <div class="card-header">
+                                Permesso di Entrare: 
+                            </div>
+                            <div class="card-body">
+                            <h5 class="card-title text-success">
+                                Concesso
+                            </h5>
+                            </div>
+                        </div>
+    
+                    </div>
+    
+                </div> 
+    
+                `;
+    
+                console.log('agrande');
+
+                // break;
+            }
         }
+
+        // Prendo un elemento dal DOM e lo inserisco in una variabile
+        const messageElement = document.getElementById('message-element');
+
+        messageElement.innerHTML = message;
+
 
     })
 
