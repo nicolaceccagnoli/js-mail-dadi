55c24287cfd21a2ce8c28f98e8e24892b5890fe9
nicolaceccagnoli <nicola.ceccagnoli.94@gmail.com>
2023-12-06 14:14:52 +0100

    Impostato Esercizio Dadi

---
+++ dadi/js/main.js
@@ -0,0 +1,10 @@
+/*
+
+1) Scrivi un programma che generi un numero da 1 a 6:
+
+    - sia per il giocatore;
+    - sia per il computer;
+
+2) Il programma dovrà stabilire il vincitore in base a chi fa il punteggio più alto.
+
+*/
---
+++ mail/README.md
@@ -0,0 +1,5 @@
+# template-html-css
+Mail
+Chiedi all'utente la sua email,
+controlla che sia nella lista di chi può accedere,
+stampa un messaggio appropriato sull'esito del controllo.
