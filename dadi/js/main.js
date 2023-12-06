a98d842a9c3c4146bb8370135796aed2bd7d8b1b
nicolaceccagnoli <nicola.ceccagnoli.94@gmail.com>
2023-12-06 14:58:41 +0100

    Edited Style and Add Event Listener to the Button

--- dadi/js/main.js
+++ dadi/js/main.js
@@ -27,7 +27,7 @@ container.append(card);
 
 // Aggiungo dello stile alla mia Card
 
-card.classList.add('card');
+card.classList.add('card', 'rounded', 'bg-dark', 'text-center', 'text-danger');
 
 // Prendo il Form dall'HTML e lo metto dentro la mia Card
 
@@ -36,8 +36,20 @@ console.log('myForm', myForm, typeof myForm);
 
 card.append(myForm);
 
+// Prendo il Bottone dall'HTML e lo dichiaro in una variabile
 
-// 1) - a) Scrivo un programma che generi un numero da 1 a 6 per il giocatore;
+const myButton = document.querySelector('button');
+console.log('myButton', myButton, typeof myButton);
+
+// Aggiungo dello stile al mio Bottone
+
+myButton.classList.add('align-center', 'btn', 'btn-outline-warning');
+
+// Faccio sì che al Click dell'utente sul Bottone il mio programma inizi a funzionare
+
+myButton.addEventListener('click', function() {
+
+    // 1) - a) Scrivo un programma che generi un numero da 1 a 6 per il giocatore;
 
 let dicePlayer = Math.floor(Math.random() * (6 - 1 + 1) ) + 1;
 console.log('dicePlayer', dicePlayer, typeof dicePlayer);
@@ -51,8 +63,20 @@ console.log('diceComputer', diceComputer, typeof diceComputer);
 // 2) Il programma dovrà stabilire il vincitore in base a chi fa il punteggio più alto.
 
 if (dicePlayer > diceComputer) {
+
+
+    // Creo un elemento in HTML
+    const victory = document.createElement('div');
+    console.log('victory', victory, typeof victory);
+
     console.log('Complimenti giocatore! Hai vinto');
+
 } else {
+
     console.log('Fai schifo! Hai perso');
 }
+
+})
+
+
 
